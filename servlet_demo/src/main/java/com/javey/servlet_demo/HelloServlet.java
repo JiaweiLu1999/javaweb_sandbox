c8615af77f05d75a8a198eeddfe898144536572e
Jiawei Lu <jl5999@columbia.edu>
2022-11-02 23:22:35 -0400

    update 1102 servlet

--- servlet_demo/src/main/java/com/javey/servlet_demo/HelloServlet.java
+++ servlet_demo/src/main/java/com/javey/servlet_demo/HelloServlet.java
@@ -25,8 +25,7 @@ public class HelloServlet extends HttpServlet {
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
-
-        request.getRequestDispatcher("login");
+        
     }
 
     public void destroy() {
--- servlet_demo/src/main/java/com/javey/util/JDBCTools.java
+++ servlet_demo/src/main/java/com/javey/util/JDBCTools.java
@@ -1,4 +1,5 @@
 package com.javey.util;
 
 public class JDBCTools {
+
 }
