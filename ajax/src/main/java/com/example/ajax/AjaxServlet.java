2df49fbfe941da7747157061a9875556a10ddf4c
Jiawei Lu <jl5999@columbia.edu>
2022-11-19 20:25:53 -0500

    update 1119 ajax

---
+++ ajax/src/main/java/com/example/ajax/AjaxServlet.java
@@ -0,0 +1,36 @@
+package com.example.ajax;
+
+
+
+import com.google.gson.Gson;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class AjaxServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        ArrayList<User> arrayList = new ArrayList<>();
+        arrayList.add(new User(0, username, password, null));
+        arrayList.add(new User(1, username, password, ""));
+
+
+        Gson gson = new Gson();
+        String s = gson.toJson(arrayList);
+
+        PrintWriter writer = response.getWriter();
+        writer.write(s);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
--- bookstore/src/main/java/com/javey/bookstore/servlet/model/UserServlet.java
+++ bookstore/src/main/java/com/javey/bookstore/servlet/model/UserServlet.java
@@ -24,6 +24,9 @@ public class UserServlet extends BaseServlet {
     }
 
     protected void regist(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String code = request.getParameter("code");
+        Object key = request.getSession().getAttribute("KAPTCHA_SESSION_KEY");
+
         //1. 获得请求参数
         Map<String, String[]> parameterMap = request.getParameterMap();
         User user = new User();
@@ -32,36 +35,50 @@ public class UserServlet extends BaseServlet {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //2. 处理注册的业务(暂时不考虑用户名重复的问题)
-        //将信息添加到数据库即可(密码的加密问题)
-        UserService userService = new UserServiceImpl();
-        boolean b = userService.regist(user);
-        //3. 给响应(页面跳转)
-        if (b) {
-            request.setAttribute("username", user.getUsername());
-            processTemplate("user/regist_success", request, response);
+
+        if (code != null && code.equals(key)) {
+            //2. 处理注册的业务(暂时不考虑用户名重复的问题)
+            //将信息添加到数据库即可(密码的加密问题)
+            UserService userService = new UserServiceImpl();
+            boolean b = userService.regist(user);
+            //3. 给响应(页面跳转)
+            if (b) {
+                request.setAttribute("username", user.getUsername());
+                processTemplate("user/regist_success", request, response);
+            }
         } else {
+            request.setAttribute("user", user);
+            request.setAttribute("code", code);
+            request.setAttribute("codeErrMsg", "Error Code!");
             processTemplate("user/regist", request, response);
         }
+
+
     }
 
     protected void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        //1. 获得参数
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        //2. 调用业务层进行业务处理
+
         UserService userService = new UserServiceImpl();
         User login = userService.login(username, password);
-        //3. 给响应
+
         if (login == null) {
             request.setAttribute("username", username);
             request.setAttribute("password", password);
             request.setAttribute("errMsg", "Wrong Username/Password!");
             processTemplate("user/login", request, response);
         } else {
-            request.setAttribute("user", login);
+            HttpSession session = request.getSession();
+            session.setAttribute("user", login);
             processTemplate("user/login_success", request, response);
         }
+    }
+
+    protected void logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.getSession().invalidate();
+        response.sendRedirect(request.getContextPath() + "/index.html");
     }
 
 
