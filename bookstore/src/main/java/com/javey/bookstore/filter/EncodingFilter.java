94b50315831ca76676d4104689cd8da14a41aaea
Jiawei Lu <jl5999@columbia.edu>
2022-11-22 16:00:54 -0500

    update 1122 bookstore

--- bookstore/src/main/java/com/javey/bookstore/filter/EncodingFilter.java
+++ bookstore/src/main/java/com/javey/bookstore/filter/EncodingFilter.java
@@ -4,7 +4,7 @@ import javax.servlet.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebFilter(filterName = "EncodingFilter", urlPatterns = "/users")
+@WebFilter(filterName = "EncodingFilter", urlPatterns = "/*")
 public class EncodingFilter implements Filter {
     public void init(FilterConfig config) throws ServletException {
     }
